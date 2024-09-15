5cc15d6cbd824f44668a59045f058897715a9458
NAJIB-B <najibajibasu@gmail.com>
2024-09-15 20:40:45 +0100

    Implemented rate limiting and throttling

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit")
+const slowDown =require("express-slow-down")
 
 const AppError = require("./utils/appError")
 const userRouter = require("./routes/userRoute")
@@ -8,6 +10,23 @@ const errorController = require("./controllers/errorController")
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  message: 'Too many request from this Ip. try again later'
+})
+
+
+const speedLimiter = slowDown({
+  windowMs: 15 * 60 * 1000,
+  delayAfter: 10,
+  delayMs: (hits) => hits * 100
+})
+
+
+
+app.use(limiter)
+app.use(speedLimiter)
 app.use(express.json())
 
 app.use("/api/v1/users", userRouter)
